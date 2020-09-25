03166a8230c5965f36c1e5390be5d104110644d1
3deep0019 <trideepghosh83@gmail.com>
2020-09-25 23:45:04 +0530

    FSJ0820B106

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,5 +1,25 @@
 package controller;
 
+import java.sql.SQLException;
+
+import dao.CrudDAO;
+import dao.CrudDaoInterface;
+import model.User;
+
 public class Main {
 
+	public static void main(String args[]) throws SQLException, ClassNotFoundException {
+
+		User user = new User();
+		user.setName("Trideep");
+		user.setEmail("Trideep@gmail.com");
+         user.setDesignation("anything");
+        user.setPassword("trideep1234");
+		
+		CrudDaoInterface crudOperations = new CrudDAO();
+		crudOperations.insert(user);
+		crudOperations.getData();
+		
+		
+	}
 }
